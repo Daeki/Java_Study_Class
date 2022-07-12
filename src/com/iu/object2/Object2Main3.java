16630af25b1be102a1800ea90c5c05349da58367
kimdaeki <kimdaeki@gmail.com>
2022-07-12 17:41:09 +0900

    object1

---
+++ src/com/iu/object2/Object2Main3.java
@@ -0,0 +1,24 @@
+package com.iu.object2;
+
+public class Object2Main3 {
+
+	public static void main(String[] args) {
+		Method3 m3 = new Method3();
+		//월급 입력
+		int salary = 5000000;
+		m3.sal(salary);
+		System.out.println(salary);
+		//m3.hap(10);
+		//
+		m3.info("iu", 30, "iu@gmail.com");
+		
+		Member member = new Member();
+		member.name="suji";
+		member.age=30;
+		member.email="suji@naver.com";
+		m3.info2(member);
+		System.out.println(member.age);
+
+	}
+
+}
